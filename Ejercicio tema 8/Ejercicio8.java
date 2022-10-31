99906d6faa1c6902d49d6752532d545f33a4fefd
Mario Morales <mmario_93@hotmail.es>
2022-10-31 15:11:40 -0600

    [ADD] ejercicio 8

---
+++ Ejercicio tema 8/Ejercicio8.java
@@ -0,0 +1,10 @@
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(29);
+        persona.setNombre("Mario");
+        persona.setTelefono(12345);
+        System.out.println(persona.toString());
+    }
+}
---
+++ Ejercicio tema 8/persona.java
@@ -0,0 +1,31 @@
+public class persona {
+    private int edad;
+    private int telefono;
+    private String nombre;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
